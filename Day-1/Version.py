a0c5535b77d5d7028f0c7caf2c5cab656d93c081
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-11-10 21:29:32 +0530

    Add files via upload

---
+++ Day-1/Version.py
@@ -0,0 +1,10 @@
+"""
+program to find version of python
+Author :Judit Jeby
+Created on 10/11/24
+"""
+import sys
+print("Python Version")
+print(sys.version)
+print("version info")
+print(sys.version_info)

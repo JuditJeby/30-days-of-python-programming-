042037c43ced9ef1b20508dbf5b4b8fc9cf13528
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-11-10 22:10:02 +0530

    Add files via upload

---
+++ Day-1/JuditJeby.py
@@ -0,0 +1,8 @@
+"""
+Program to print out a greeting
+Author :Judit Jeby
+Cteated on 10/11/24
+"""
+name =input("Enter your name : ")
+age=int(input("Enter your age: "))
+print(f"Hello{name} !,You are {age} years old")

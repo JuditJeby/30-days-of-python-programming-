ef135549489b44db457698f17ef92ac10e3635c5
Kenaz Mathukutty <kenazmathukutty13@gmail.com>
2024-11-11 23:22:37 +0530

    Arithmetic(+,-,*)

---
+++ Day-2/kenaz(Arithmetic).py
@@ -0,0 +1,12 @@
+'''
+Python program of arithmetic
+Atuthor:Kenaz mathukutty
+Created:11/11/2024
+'''
+
+add=5+10
+sub=5-10
+multi=5*10
+print("Addition(5+10)=",add)
+print("Substration(5-10)=",sub)
+print("Multiplication(5*10)=",multi)

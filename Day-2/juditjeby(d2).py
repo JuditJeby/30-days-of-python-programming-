bc5abb52e3a3dcd95a0a024b272c9411b999b4b7
JuditJeby <juditjeby2028@cs.sjcetpalai.ac.in>
2024-11-11 22:03:00 +0530

    Add files via upload

---
+++ Day-2/juditjeby(d2).py
@@ -0,0 +1,17 @@
+"""
+Program performing basic arithmetic operation
+Author :Judit Jeby
+Cteated on 10/11/24
+
+
+"""
+
+
+num1=5
+num2=10
+addition=num1+num2
+print(addition)
+subtraction=num1-num2
+print(subtraction)
+multiplication=num1*num2
+print(multiplication)

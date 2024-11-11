29e5c68938e6f9962c1739de6360dc663e73737f
Liya408 <liyabn80@gmail.com>
2024-11-12 00:06:12 +0530

    Add files via upload

---
+++ Day-2/Liya(d2-arithemetic operations).py
@@ -0,0 +1,11 @@
+"""Author:Liya Binu
+Date:11-11-2024
+program to perform the basic arithemetic operations"""
+num1=10
+num2=5
+add=num1+num2
+print(add)
+sub=num1-num2
+print(sub)
+multiplication=num1*num2
+print(multiplication)

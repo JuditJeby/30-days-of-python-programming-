15cd3e5c1da6771c7404d04b41b108169777c5c4
Liya408 <liyabn80@gmail.com>
2024-11-11 23:50:36 +0530

    Add files via upload

---
+++ Day-1/Liya BInu(Greeting).py
@@ -0,0 +1,6 @@
+"""Author:Liya Binu
+date:11-11-2024
+program to greets the user"""
+name=(input("What's your name?"))
+age=(input("How old are you?"))
+print("Hello,",name,"! you are",age,"years old")

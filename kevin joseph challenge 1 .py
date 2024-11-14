f440248c310cd882c55553e19b00372e4c356954
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-11-14 08:21:09 +0530

    Create kevin joseph challenge 1 .py

---
+++ kevin joseph challenge 1 .py
@@ -0,0 +1,6 @@
+'''
+python program to find name age and greeting
+author: kevin joseph'''
+name_=input("enter your name:")
+age_=input("enter your age:")
+print(f"hello",name_,"!","you are",age_,"years old.","good morning")

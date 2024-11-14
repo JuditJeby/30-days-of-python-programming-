6600a82fc9f04d782884683c3b092c5ce62e739f
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-11-14 08:23:21 +0530

    Create kevin joseph challenge 2 .py

---
+++ Day-2/kevin joseph challenge 2 .py
@@ -0,0 +1,10 @@
+'''
+program to find the version of python
+Author : kevin joseph
+date:10/11/2024
+'''
+import sys
+print("Python Version")
+print(sys.version)
+print("version info")
+print(sys.version_info)

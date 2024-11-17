4e3a10fc383bfd9d3d83c26cd9e32785c8fb6d3c
Lakshmy1907 <lakshmybyju2028@cs.sjcetpalai.ac.in>
2024-11-17 22:01:24 +0530

    Add files via upload

---
+++ Day 8/lakshmy area of circle.py
@@ -0,0 +1,8 @@
+''' author:Lakshmy Byju
+   date:16-11-2024
+   python program to check a num is amstrog
+'''
+import math
+radius=int(input("Enter the area of a circle"))
+area=math.pi*(radius**2)
+print(f"area of a circle with radius{radius} is :{area:.2f} square unit")

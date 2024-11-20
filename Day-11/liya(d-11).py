2304dd6bc7b6d414c45303bea52dfc971a95e16e
Liya408 <liyabn80@gmail.com>
2024-11-20 22:27:38 +0530

    Update liya(d-11).py

--- Day-11/liya(d-11).py
+++ Day-11/liya(d-11).py
@@ -1,3 +1,5 @@
-my_list=[1,2,3,4,5]
+"""Author:Liya Binu
+Date:20-11-2024
+program to interchange the first and last elements"""my_list=[1,2,3,4,5]
 my_list[0],my_list[-1]=my_list[-1],my_list[0]
 print("list after swapping the first and last digits is",my_list)

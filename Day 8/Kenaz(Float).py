41e12e308933a4d851a802db66045c33a2aec2ae
Kenaz Mathukutty <kenazmathukutty13@gmail.com>
2024-11-20 21:57:50 +0530

    Float

---
+++ Day 8/Kenaz(Float).py
@@ -0,0 +1,9 @@
+'''
+Pyhon program to find Compound interest
+Author: Kenaz mathukutty
+Created: 20/11/2024
+'''
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * (radius ** 2)
+print(f"The area of the circle with radius {radius} is: {area:.2f} square units")

135e588d3dfe790b4d0a09261a25dfeadff4b55e
Kenaz Mathukutty <kenazmathukutty13@gmail.com>
2024-11-20 21:54:27 +0530

    Armstrong number

---
+++ Day - 7/Kenaz(Armstrong number).py
@@ -0,0 +1,17 @@
+'''
+Pyhon program to find Compound interest
+Author: Kenaz mathukutty
+Created: 20/11/2024
+'''
+def is_armstrong(number):
+    digits = str(number)
+    power = len(digits)
+    total = sum(int(digit) ** power for digit in digits)
+    return total == number
+number = int(input("Enter a number: "))
+if is_armstrong(number):
+    print(f"{number} is an Armstrong number.")
+else:
+    print(f"{number} is not an Armstrong number.")
+
+

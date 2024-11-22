1d334e2d669b57f098df0482924d34741ca2b1be
Kenaz Mathukutty <kenazmathukutty13@gmail.com>
2024-11-22 19:52:33 +0530

    interchange element

---
+++ Day-11/kenaz(interchange).py
@@ -0,0 +1,9 @@
+'''
+python program
+Author: Kenaz mathukutty
+Created: 22/11/2024
+'''
+my_list = [1, 2, 3, 4, 5]
+print("Original List:", my_list)
+my_list[0], my_list[-1] = my_list[-1], my_list[0]
+print("Swapped List:", my_list)
